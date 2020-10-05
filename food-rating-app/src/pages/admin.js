fb9458ca7626b5abca890c5be07d2dbe7e5408c7
tester <aiforeveryoneai@gmail.com>
2020-10-05 12:48:38 +0500

    improved ui of admin.js

--- food-rating-app/src/pages/admin.js
+++ food-rating-app/src/pages/admin.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import bootstrap from "bootstrap";
 import cn from "classnames";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
@@ -19,13 +20,26 @@ const AdminPanel = () => {
   const users = [{ name: "Talha" }, { name: "Ali" }];
   return (
     <Container className="pt-5">
-      <Row className="d-flex justify-content-around">
-        <Col className="border border-primary col mx-5">
+      <Row className="d-flex" style={{ height: "70vh" }}>
+        <Col className="shadow-lg bg-white rounded mx-5">
           {users.map((user) => (
-            <Row className="py-2 px-2">{user.name}</Row>
+            <Row
+              className="shadow my-3 mx-2 py-2 px-2 rounded font-weight-bold"
+              style={{ height: "7vh" }}
+            >
+              {user.name}
+              <div className="btn-group ml-auto">
+                <a href="#">
+                  <button className="btn btn-primary btn-sm  mx-2">Edit</button>
+                </a>
+                <a href="#">
+                  <button className="btn btn-danger btn-sm">Delete</button>
+                </a>
+              </div>
+            </Row>
           ))}
         </Col>
-        <Col className="border border-primary vh-50 mx-5">
+        <Col className="shadow-lg bg-white rounded mx-5">
           <h1>
             list list list list list list list list list list list list list
           </h1>
